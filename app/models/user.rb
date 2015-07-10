80bf1d40afd4d40b0796965be71ab02cf850f4b0
Minyoung Jun <minyoung9353@gmail.com>
2015-07-10 15:32:03 +0900

    ww

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,11 @@ class User < ActiveRecord::Base
     def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
+        user.name = auth.info.name
         user.uid = auth.uid
         user.email = auth.info.email
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = auth.credentials.expires_at
         user.password = Devise.friendly_token[0,20]
       end
     end
