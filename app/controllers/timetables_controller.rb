252d837ca712f875087a86204929c6f4ee4e8264
Minyoung Jun <minyoung9353@gmail.com>
2015-07-10 17:35:38 +0900

    minyoung

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -2,7 +2,12 @@ class TimetablesController < ApplicationController
 
   def set_table
 
-    
+    @time_array = ["오전 9시", "오전 10시", "오전 11시", "정오(12시)", "오후 1시", "오후 9시", "오후 10시", "오후 11시", "자정(12시)"]
+
+
+
+
+
 
   end
 end
