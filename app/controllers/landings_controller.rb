9c6e561828af4bdd6a771c903abbd5f5280ef514
Minyoung Jun <minyoung9353@gmail.com>
2015-07-21 18:26:16 +0900

    w

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,13 +2,11 @@ class LandingsController < ApplicationController
 
 
   before_filter :detect_korea
+  require 'geoip'
 
   def index
 
-    require 'geoip'
     @main = true
-    geo = GeoIP.new(Rails.root.join('public', 'GeoIP.dat')).country(request.remote_ip)
-    render :text => geo.inspect
 
   end
 end
