4e42423f5541443b3a5c097241cbd7971fbe791c
Minyoung Jun <minyoung9353@gmail.com>
2015-08-06 17:10:26 +0900

    cases

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -7,6 +7,7 @@ class LandingsController < ApplicationController
   def index
 
     @main = true
+    @cases = [["Coffee", "coffee.jpg"], ["Drone", "drone.jpg"],["Elon Musk","elon.jpg"],["Google vs Amazon", "googlevsamazon.png"],["House of Cards","house.jpg"], ["Icecream", "icecream.jpg"], ["MLB", "mlb.png"],["Shale Gas", "shale.jpg"],["Netflix", "netflix.jpg"],["Smartphone","smartphone.jpg"],["Social Enterprises", "social.png"]]
 
   end
 end
